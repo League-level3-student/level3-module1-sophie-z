50ff39d18217740c63de65c4e88745ffe3dc9447
sophie-z <sophie.zeng2@gmail.com>
2020-12-29 18:41:44 -0800

    .

--- src/_00_Dynamic_Integer_Array/DynamicIntegerArray.java
+++ src/_00_Dynamic_Integer_Array/DynamicIntegerArray.java
@@ -41,26 +41,33 @@ public class DynamicIntegerArray {
 	public void set(int v, int location) {
 		//A. set the variable at the location passed in to the method
 		//   to the new value v
+		ints[location] = v;
 	}
 	
 	//7. Complete the steps in the insert method
 	public void insert(int v, int location) {
 		//A. create and initialize a new int array to be one 
 		//   element longer than the member array
-		
+		int[] ints3 = new int[ints.length+1];
 		//B. Make a for loop that iterates through the new array
-
+		for(int i = 0; i<ints3.length; i++) {
 			//C. if i is less than location:
 		    //		set the element at i of the new array to the element at i of the member array
-			
+			if(i<location) {
+				ints3[i] = ints[1];
+			}
 			//D. else if i is greater than location:
 		    //		set the element at i of the new array to the i - 1 element of the member array
-			
+			else if(i>location) {
+				ints3[i] = ints[i-1];
+			}
 			//E. else, set the element at i of the new array to the value v
-		
-		
+			else {
+				ints3[i] = v;
+			}
+		}
 		//F. set the member array equal to the new array
-	
+		ints = ints3;
 	}
 	
 	//8. Run the tests again and check your progress
@@ -68,30 +75,42 @@ public class DynamicIntegerArray {
 	//9. Complete the steps in the remove method
 	public void remove(int location) {
 		//A. create a new array that is one element smaller than the member array
-		
+		int[] ints4 = new int[ints.length-1];
 		//B. make a for loop to iterate through the member array
-		
+			for(int i = 0; i<ints.length; i++) {
 			//C. if i  is less than location
 			//		set the element at i of the new array to the element at i of the member array
-			
+				if(i<location) {
+					ints4[i] = ints[1];
+				}
 			//D. else if i  is greater than location
 			//		set the element at i - 1 of the new array to the element at i of the member array
-			
+				else if(i>location) {
+					ints4[i-1] = ints[i];
+				}
 			//E. else, continue;
-			
+				else {
+					
+				}
+			}
 		//F. set the member array equal to the new array
+			ints = ints4;
 	}
 	
 	//10. Run the tests again the see if you are correct so far
 	
 	//11. Complete the size method so that it returns the length of the member array.
 	public int size() {
-		return 0;
+		return ints.length;
 	}
 	
 	//12. Complete the clear array so that it sets the member array 
 	//    equal to a new integer array of size 0
 	public void clear() {
+		int[] ints5 = new int[0];
+		for(int i = 0; i<ints5.length; i++) {
+			ints[i] = ints5[i];
+		}
 	}
 	
 	//13. Run the test again to see if you are finished.
--- src/_03_IntroToStacks/_01_IntroToStack.java
+++ src/_03_IntroToStacks/_01_IntroToStack.java
@@ -9,13 +9,35 @@ public class _01_IntroToStack {
 	public static void main(String[] args) {
 		//1. Create a Stack of Doubles
 		//   Don't forget to import the Stack class
-		
+		Stack<Double> doubles = new Stack<Double>();
 		//2. Use a loop to push 100 random doubles between 0 and 100 to the Stack.
-		
+		for(int i = 0; i<100; i++) {
+			double Random = new Random().nextDouble();
+			doubles.push(Random);
+		}
 		//3. Ask the user to enter in two numbers between 0 and 100, inclusive. 
-		
+		String answer1 = JOptionPane.showInputDialog("Enter a number between 0 and 100, inclusive.");
+		String answer2 = JOptionPane.showInputDialog("Enter a different number between 0 and 100, inclusive.");
 		//4. Pop all the elements off of the Stack. Every time a double is popped that is
-		//   between the two numbers entered by the user, print it to the screen.
+		//   between the two numbers entered by the user, print it to the screen.) 
+		int number1 = Integer.parseInt(answer1);
+		int number2 = Integer.parseInt(answer2);
+		int smaller;
+		int bigger;
+		if(number1<number2) {
+			smaller = number1;
+			bigger = number2;
+		}
+		else if(number1>number2) {
+			smaller = number2;
+			bigger = number1;
+		}
+		for(int i = 0; i<100; i++) {
+			doubles.pop();
+			if(doubles>smaller && doubles<bigger) {
+				
+			}
+		}
 		
 		
 		//   EXAMPLE:
