9fbed63c6c5a60616ee3fdc5e6f85c412bf05947
sophie-z <sophie.zeng2@gmail.com>
2020-12-29 19:56:54 -0800

    .

--- src/_03_IntroToStacks/_02_TextUndoRedo.java
+++ src/_03_IntroToStacks/_02_TextUndoRedo.java
@@ -1,6 +1,14 @@
 package _03_IntroToStacks;
 
-public class _02_TextUndoRedo {
+import java.awt.Component;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class _02_TextUndoRedo implements KeyListener {
 	/* 
 	 * Create a JFrame with a JPanel and a JLabel.
 	 * 
@@ -13,6 +21,49 @@ public class _02_TextUndoRedo {
 	 * off the Stack and added back to the JLabel.
 	 * 
 	 * */
+	
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JLabel label = new JLabel();
+	
+	void setup(){
+		panel.add(label);
+		frame.setSize(300, 300);
+		frame.add(panel);
+		frame.addKeyListener(this);
+		frame.setVisible(true);
+		frame.pack();
+	}
+
+	public static void main(String[] args) {
+		 _02_TextUndoRedo tur = new _02_TextUndoRedo();
+		 tur.setup();
+	}
+	
+	String key = "";
+	
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		char keypressed = arg0.getKeyChar();
+		key = key + Character.toString(keypressed);
+		if(key.equals("backspace")) {
+			
+		}
+		label.setText(key);
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 	
 }
